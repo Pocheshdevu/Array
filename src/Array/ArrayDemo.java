2ab2269d4e7f16f88a1b1f5f9ee2f005ff6b82b4
Pochesh devu <pocheshd@gmail.com>
2022-02-06 12:03:07 +0530

    first commit

---
+++ src/Array/ArrayDemo.java
@@ -0,0 +1,33 @@
+package Array;
+
+
+import java.util.Scanner;
+
+//declare array with size without elements
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		int array[]=new int[10];
+		
+		System.out.println("Store elements in this array dynamically");
+		Scanner sc=new Scanner(System.in);
+		
+		for(int i=0;i<array.length;i++)
+		{
+			System.out.println("Enter any number");
+			array[i]=sc.nextInt();
+		}
+		
+		System.out.println("Let's print elements of array");
+		for(int a:array)
+		{
+			System.out.println(a);
+		}
+		
+	}
+		
+}
+
+
+
